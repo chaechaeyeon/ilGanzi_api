1e08792167aeaca8a1d3028dc118c68ffb26681f
sm4640 <s1m0611@naver.com>
2023-11-04 23:41:45 +0900

    유저모델 세팅

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'last_login', 'treename', 'treephase', 'watered', 'tutorial', 'created_at', 'updated_at')
