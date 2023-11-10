33f695a0aaecab94f9c45c8541f6edb7566ccbe6
sm4640 <s1m0611@naver.com>
2023-11-11 00:22:22 +0900

    watered 수정중

--- accounts/operator.py
+++ accounts/operator.py
@@ -9,7 +9,7 @@ def start():
     scheduler=BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    @scheduler.scheduled_job('cron', hour=0, minute=7, name = 'resetWatered')
+    @scheduler.scheduled_job('cron', hour=0, minute=22, name = 'resetWatered')
     def auto_resetWatered():
         resetWatered()
     scheduler.start()
